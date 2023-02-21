9ba53e636977846875b63fdcf9d9d58495c70263
Kenta <106167071+Its-Kenta@users.noreply.github.com>
2023-02-21 22:02:36 +0000

    Update parser.c

--- parser.c
+++ parser.c
@@ -11,7 +11,6 @@ static char buffer[2048];
 char* readline(char* prompt) {
   fputs(prompt, stdout);
   fgets(buffer, 2048, stdin);
-  //char* cpy = malloc(strlen(buffer)+1);
   char* cpy = malloc(strlen(buffer)+1);
   strcpy(cpy, buffer);
   cpy[strlen(cpy)-1] = '\0';
